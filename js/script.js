636a80cf5d36f8bddec00065f60553a45de38fe3
Andrey Bondarenko <Andrew-Bondarenko93@mail.ru>
2025-05-16 00:06:30 +0300

    2-nd commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,24 @@ new Swiper(".swiper-container", {
         clickable: true
       },
 });
+
+let btn = document.querySelector('.button-show-more');
+let desktop = document.querySelector('.block-desktop');
+let img = document.querySelector('.icon-btn');
+let text = document.querySelector('.text-btn');
+let isShow = false;
+
+btn.addEventListener('click', function() {
+  if (isShow) {
+    desktop.classList.remove("block-desktop-show");
+    btn.classList.remove("button-show-more-reverse");
+    text.innerHTML = "Показать всё";
+    img.classList.remove("icon-btn-reverse");
+  } else {
+    desktop.classList.add("block-desktop-show");
+    btn.classList.add("button-show-more-reverse");
+    text.innerHTML = "Скрыть";
+    img.classList.add("icon-btn-reverse");
+  }
+   isShow = !isShow;
+})
